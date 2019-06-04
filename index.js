edb4f9b91e96a77048287988e47409b0769ee873
YoserEstrada <yestradahernandez14@correo.unicordoba.edu.co>
2019-06-04 22:31:03 +0000

    nodemon

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { app } = require("./bin/routes");
 app.listen(3000, () => {
-  console.log("Server on port http://localhost:3000");
+  console.log("Server on port http://localhost:3000" );
 });

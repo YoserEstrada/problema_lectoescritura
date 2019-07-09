0db0537e8358cdd9a7fe7051b1103006ca26a450
YoserEstrada <yestradahernandez14@correo.unicordoba.edu.co>
2019-07-09 02:37:18 +0000

    set y get

--- bin/Controller.js
+++ bin/Controller.js
@@ -22,7 +22,7 @@ class Controller{
             console.error(e)
         }
     }
-
+    //traer todos los funcionarios
     getFuncionarios(res){
         Funcionario.find({}, (err,funcionarios)=>{
             if(err) throw err;
@@ -30,6 +30,22 @@ class Controller{
             })
             }
 
+    //Agregar un funcionario
+    setFuncionario(Funcionario, res) {
+        Funcionario.create(Funcionario, function(err, newFuncionario) {
+            if (err) throw err;
+            res.send({ status: 200 });
+            });
+            }
+
+    //Traer un funcionario por su id
+        getFuncionarios(idfuncionario, res) {
+            Funcionario.find({ _id: idfuncionario }, (err, funcionario) => {
+            res.send({ status: 200, funcionario: funcionario });
+            });
+            }
+
+    //traer todos los estudiantes
     getEstudiantes(res){
             Estudiante.find({}, (err,estudiantes)=>{
             if(err) throw err;
@@ -37,13 +53,29 @@ class Controller{
             })
             }
 
+    //Agregar un estudiante
+    setEstudiante(Estudiante, res) {
+        Estudiante.create(Estudiante, function(err, newEstudiante) {
+            if (err) throw err;
+            res.send({ status: 200 });
+            });
+            }
+
+    //Traer un estudiante por su id
+        getEstudiante(idestudiante, res) {
+            Estudiante.find({ _id: idestudiante }, (err, estudiante) => {
+            res.send({ status: 200, estudiante: estudiante });
+            });
+            }
+
+    //traer todas las asignaturas
     getAsignaturas(res){
             Asignatura.find({}, (err,asignaturas)=>{
             if(err) throw err;
             res.send( asignaturas );
             })
             }
-
+    //traer todos los problemas
     getProblemas(res){
             Problema.find({}, (err,problemas)=>{
             if(err) throw err;
@@ -51,13 +83,42 @@ class Controller{
             })
             }
 
+    //Agregar un problema
+    setProblema(Problema, res) {
+        Problema.create(Problema, function(err, newProblema) {
+            if (err) throw err;
+            res.send({ status: 200 });
+            });
+            }
+
+    //Traer un problema por su id
+        getProblema(idtipodeproblema, res) {
+            Problema.find({ _id: idtipodeproblema }, (err, problema) => {
+            res.send({ status: 200, problema: problema });
+            });
+            }
+    //traer todos los responsables
     getResponsables(res){
             Responsable.find({}, (err,responsables)=>{
             if(err) throw err;
             res.send( responsables );
             })
             }
+    //Agregar un responsable
+    setResponsable(Responsable, res) {
+        Responsable.create(Responsable, function(err, newResponsable) {
+            if (err) throw err;
+            res.send({ status: 200 });
+            });
+            }
 
+    //Traer un responsable por su id
+        getResponsable(idresponsable, res) {
+            Responsable.find({ _id: idresponsable }, (err, responsable) => {
+            res.send({ status: 200, responsable: responsable });
+            });
+            }
+    //traer todos los reportes
     getReportes(res){
             Reporte.find({}, (err,reportes)=>{
             if(err) throw err;
--- bin/routes.js
+++ bin/routes.js
@@ -1,33 +1,90 @@
 const express = require("express");
 const app = express();
 const { controller } = require("./Controller");
+const bodyParser = require("body-parser");
 
+//pagina principal
 app.get("/", (req, res) => {
   res.send("problema_lectoescritura");
 });
 
+//traer todos los funcionarios
 app.get("/Funcionarios", (request, res) => {
 controller.getFuncionarios(res);
 });
 
+//Agregar un funcionario
+app.post("/Funcionarios", function(req, res) {
+  let { Funcionario } = req.body;
+  controller.setFuncionario(Funcionario, res);
+});
+
+//Traer un funcionario por su id
+app.get("/Funcionarios/:idfuncionario", function(req, res) {
+  let { idfuncionario } = req.params;
+  controller.getFuncionarios(idfuncionario, res);
+});
+
+//traer todos los estudiantes
 app.get("/Estudiantes", (request,res)=>{
 controller.getEstudiantes(res);
 });
 
+//Agregar un estudiante
+app.post("/Estudiantes", function(req, res) {
+  let { Estudiante } = req.body;
+  controller.setEstudiante(Estudiante, res);
+});
+
+//Traer un estudiante por su id
+app.get("/Estudiantes/:idestudiante", function(req, res) {
+  let { idestudiante } = req.params;
+  controller.getEstudiantes(idestudiante, res);
+});
+
+//traer todas las asignaturas
 app.get("/Asignaturas", (request,res)=>{
 controller.getAsignaturas(res);
 });
 
+//traer todos los problemas
 app.get("/Problemas", (request,res)=>{
 controller.getProblemas(res);
 });
 
+//Agregar un problema
+app.post("/Problemas", function(req, res) {
+  let { Problema } = req.body;
+  controller.setProblema(Problema, res);
+});
+
+//Traer un problema por su id
+app.get("/Problemas/:idtipodeproblema", function(req, res) {
+  let { idtipodeproblema } = req.params;
+  controller.getProblemas(idtipodeproblema, res);
+});
+
+//traer todos los responsables
 app.get("/Responsables", (request,res)=>{
 controller.getResponsables(res);
 });
 
+//Agregar un responsable
+app.post("/Responsables", function(req, res) {
+  let { Responsable } = req.body;
+  controller.setResponsable(Responsable, res);
+});
+
+//Traer un responsable por su id
+app.get("/Responsables/:idresponsable", function(req, res) {
+  let { idresponsable } = req.params;
+  controller.getResponsables(idresponsable, res);
+});
+
+//traer todos los reportes
 app.get("/Reportes", (request,res)=>{
 controller.getReportes(res);
 });
+
 
 exports.app = app;
